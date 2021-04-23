fb25711c34d837da0d965224bf389bccf818b170
NikhilMJagtap <nikhil.jagtap@spit.ac.in>
2021-04-23 22:07:18 +0530

    updated test for flow table

--- test/test_flow_table.py
+++ test/test_flow_table.py
@@ -1,16 +1,19 @@
 import unittest
 from pyplico.packetReader import PacketReader
 from pyplico.flowtable import FlowTable
+from pyplico.smtp_utils import SMTPUtils
 
 class TestFlowTable(unittest.TestCase):
     def test_flow_table(self):
-        pr = PacketReader("../src/data/sample.pcap", to_itr=False, to_list=True, sort=True)
+        pr = PacketReader("../src/data/smtp.pcap", to_itr=False, to_list=True, sort=True)
         ft = FlowTable()
         for packet in pr.packets:
             try:
                 ft.push(packet[0])
             except ValueError:
                 pass
+
+        SMTPUtils.hunt_credentials(ft, connection="192.168.1.4__217.12.11.66__1470__587")
 
 if __name__ == "__main__":
     unittest.main()
