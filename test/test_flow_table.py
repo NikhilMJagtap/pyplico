287252011affbb9e37b1dfc98a0fb39a069c9579
NikhilMJagtap <nikhil.jagtap@spit.ac.in>
2021-05-01 19:59:06 +0530

    added flow table as a part of packetreader

--- test/test_flow_table.py
+++ test/test_flow_table.py
@@ -1,19 +1,11 @@
 import unittest
 from pyplico.packetReader import PacketReader
-from pyplico.flowtable import FlowTable
 from pyplico.smtp_utils import SMTPUtils
 
 class TestFlowTable(unittest.TestCase):
     def test_flow_table(self):
-        pr = PacketReader("../src/data/smtp.pcap", to_itr=False, to_list=True, sort=True)
-        ft = FlowTable()
-        for packet in pr.packets:
-            try:
-                ft.push(packet[0])
-            except ValueError:
-                pass
-
-        
+        pr = PacketReader("../src/data/smtp.pcap", to_itr=False, to_list=True, to_ft=True, sort=True)
+        ft = pr.get_flow_table()
 
 if __name__ == "__main__":
     unittest.main()
